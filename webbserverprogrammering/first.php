06cdb28c6284af73ff446f9c11437635d19703ba
olbo98 <ob655088@gmail.com>
2016-08-25 15:30:11 +0200

    skapade first.php

---
+++ webbserverprogrammering/first.php
@@ -0,0 +1,23 @@
+<!doctype html>
+
+<html>
+    
+    <head>
+        
+        <meta charset="utf-8">
+        
+        <title>My first php-file</title>
+        
+    </head>
+    
+    <body>
+        
+        <?php
+        
+            echo "<h1>Hello fredagsmys</h1>";
+        
+        ?>
+        
+    </body>
+    
+</html>
